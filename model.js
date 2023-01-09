ef9ccfd8a344ab0e4edc5c90f650d9339c1630c5
opelik417 <opelik417@gmail.com>
2023-01-09 10:10:37 +0300

    исправляет версию вк

--- model.js
+++ model.js
@@ -12,7 +12,7 @@ export default{
         });
     },
     callApi(method,params){
-        params.v = params.v || '5.78';
+        params.v = params.v || '5.81';
 
         return new Promise((resolve, reject) =>{
             VK.api(method, params, response =>{
